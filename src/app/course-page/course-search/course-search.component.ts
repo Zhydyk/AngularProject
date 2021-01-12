a5b535a106ff33a200267de03dd908bfcaa9ebc8
Roman Zhydyk <Roman_zhydyk@epam.com>
2021-01-12 18:54:07 +0200

    Fix pull request comments

--- src/app/course-page/course-search/course-search.component.ts
+++ src/app/course-page/course-search/course-search.component.ts
@@ -1,17 +1,33 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'course-search',
   templateUrl: './course-search.component.html',
   styleUrls: ['./course-search.component.scss'],
 })
-export class CourseSearchComponent {
+export class CourseSearchComponent implements OnInit{
   public courseSearch: string;
+  public subject = new Subject<string>();
 
   @Output()
-  public search: EventEmitter<string> = new EventEmitter<string>();
+  public search: EventEmitter<string> = new EventEmitter<string>(true);
+
+  public ngOnInit(): void {
+    this.subject.pipe(
+      debounceTime(1000),
+      filter((value) => !value || value.length > 3),
+     ).subscribe(
+      (value) => this.search.emit(value)
+     )
+  }
 
   public searchCourse(): void {
-    this.search.emit(this.courseSearch);
+    this.subject.next(this.courseSearch);
+  }
+
+  public ngOnDestroy(): void {
+    this.subject.unsubscribe();
   }
 }
